8182fa738ee93b50e4d68fbf4d3ac3330180f434
clarawisenfath <cx022007@gmail.com>
2024-10-28 16:56:37 -0300

    Implementando a leitura do csv

--- tf_agrupamento.py
+++ tf_agrupamento.py
@@ -10,18 +10,20 @@ Original file is located at
 #importando as bibliotecas necessarias
 import numpy as np
 from sklearn.cluster import KMeans
+import pandas as pd
+
+#1 passo: especificar o caminho do arquivo CSV
+caminho_arquivo = '/content/filmes_100_usuarios.csv'
+
+#2 passo: ler o CSV usando pandas
+df = pd.read_csv(caminho_arquivo)
+
+#exibir o cabeçalho do arquivo para verificar se foi lido corretamente
+print(df.head())
 
 #matriz com os filmes assistidos
-filmes_assistidos = np.array([
-     [1,0,0,1],
-     [1,1,0,0],
-     [0,1,1,0],
-     [0,0,1,1],
-     [1,0,1,0],
-     [0,1,0,1],
-     [0,1,0,1],
-     [0,1,0,1]
-])
+#convertendo o Dataframe para uma matriz numpy
+filmes_assistidos = df.drop(columns=["Unnamed: 0"]).values
 
 #definindo numeros de cluster(grupos)
 num_cluster = 2
@@ -72,7 +74,7 @@ def recomendar_filme(filmes, filmes_assistidos, grupos_indice):
   return sorted(filmes_recomendados)
 
 #exemplo de uso da função recomendar_filmes
-filmes_assistidos_usuario = [1, 0, 1, 0] #vetor de filme
+filmes_assistidos_usuario = [1, 0, 0, 1, 1, 1, 1, 0, 0, 1] #vetor de filme
 #assistidos (por exemplo, assistiu aos filmes 1 e 3)
 filmes_recomendados = recomendar_filme(filmes_assistidos_usuario, filmes_assistidos, grupos_indice)
 
